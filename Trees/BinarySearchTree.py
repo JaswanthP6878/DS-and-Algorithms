775e27cb7b4a1d0749854e819b882ac089d5726a
JaswanthP6878 <klucse2000039034@gmail.com>
2023-03-21 11:06:54 +0530

    added trees

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -3,12 +3,12 @@ class BSTNode:
         self.val = val
         self.left = None
         self.right = None
+
 class BstTree:
     def __init__(self):
         self.root = None
-    
-    def insert(self,key):
 
+    def insert(self,key):
         def util(root, key):
             if root == None:
                 return BSTNode(key)
@@ -17,11 +17,9 @@ class BstTree:
             if key > root.val:
                 root.right = util(root.right, key)
             return root
-    
         self.root = util(self.root, key)
-
+    
     def inorder(self):
-
         def inorder_util(root):
             if root:
                 inorder_util(root.left)
@@ -43,29 +41,43 @@ def delete_root(root,key):
     elif key > root.val:
         root.right = delete_root(root.right, key)
     else:
-        if root.left is None:
+        if root.left is None: 
+            '''case where we have root right'''
             ptr = root.right
             root = None
             return ptr
         elif root.right is None:
+            '''case where we have root left'''
             ptr = root.left
             root = None
             return ptr
+        '''case where we have both left and right i.e 2 children 1 and no child case are taken care
+            above
+        '''
         ptr = inorderSuccesor(root.right)
         root.val = ptr.val
         root.right = delete_root(root.right, ptr.val)
     return root
 
+def max_val(root):
+    temp = root
+    while temp.right:
+        temp = temp.right
+    return temp.val
+
+
 btree = BstTree()
 btree.insert(2)
 btree.insert(6)
 btree.insert(3)
+btree.insert(15)
 btree.insert(8)
+btree.insert(16)
 btree.insert(9)
 btree.insert(4)
 
-btree.inorder()
+# btree.inorder()
 print()
-btree.root = delete_root(btree.root,6)
-btree.inorder()
-        
+# btree.root = delete_root(btree.root,6)
+# btree.inorder()
+print(max_val(btree.root))
---
+++ tricky_questions/nearestMinElement.py
@@ -0,0 +1,14 @@
+from Trees import BinarySearchTree as BST
+
+x = [1,2,3,4,5]
+x = [3,7,4,6,2,6]
+
+def findMin(x):
+    tree1 =BST.BstTree
+    tree1.insert(-1)
+    ans = []
+    for i in x:
+        pass
+    
+
+
